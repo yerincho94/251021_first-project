034b62cfb663e5519347c1ceed8ae5930f53a376
yerincho94 <yerincho94@gmail.com>
2025-10-21 12:11:57 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    // 실행하려고 하면 main() 메서드가 필요
+    public static void main(String[] args) {
+        //출력
+        //System -> out -> print
+        System.out.println("Hello World!");
+        // Eclipse -> sysout
+        // IntelliJ -> sout
+        System.out.println("Hello IntelliJ!");
+
+    }
+}
